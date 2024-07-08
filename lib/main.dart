a054a7d4b8eec7a69fac56253a8400250dfe3e1e
salimazeriDev <salimazeri.dev@gmail.com>
2024-07-08 15:50:32 +0200

    chore: project setup

---
+++ lib/main.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      home: MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatelessWidget {
+  const MyHomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text('Title')),
+      body: const Center(
+        child: Column(
+          children: [
+            FlutterLogo(),
+            FlutterLogo(),
+          ],
+        ),
+      ),
+    );
+  }
+}
